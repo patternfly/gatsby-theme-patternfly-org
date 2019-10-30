1edfee864ac72658db48812dfffdb918fadc9bf7
evwilkin <evan.wilkinson@gmail.com>
2019-10-30 16:14:29 -0400

    fix(docs): refactored code to add blog to top nav

--- gatsby-theme-patternfly-org/components/topNav.js
+++ gatsby-theme-patternfly-org/components/topNav.js
@@ -8,23 +8,15 @@ const TopNav = ({ location, context, navItems }) => {
     <Nav aria-label="Nav">
       <NavList variant="horizontal" className="ws-top-nav">
         {navItems.map(({ path, text, contexts }) => (
-          (path.includes('http'))
-            ? (
-              <NavItem
-                key={path}
-                isActive={location.pathname.includes(path) || (contexts || []).includes(context)}
-                >
-                <a href={path} className="ws-topnav-blog" target="_blank">{text}</a>
-              </NavItem>
-            )
-            : (
-              <NavItem
-                key={path}
-                isActive={location.pathname.includes(path) || (contexts || []).includes(context)}
-                >
-                <Link to={path}>{text}</Link>
-              </NavItem>
-            )
+          <NavItem
+            key={path}
+            isActive={location.pathname.includes(path) || (contexts || []).includes(context)}
+          >
+            {path.includes('//')
+              ? <a href={path} target="_blank">{text}</a>
+              : <Link to={path}>{text}</Link>
+            }
+          </NavItem>
         ))}
       </NavList>
     </Nav>
