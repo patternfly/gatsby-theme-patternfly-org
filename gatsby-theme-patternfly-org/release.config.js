0a956a91b9fa959a09cd79dd819e4f18dc09ed53
redallen <zallen@redhat.com>
2019-11-15 15:11:07 -0500

    release all assets

--- gatsby-theme-patternfly-org/release.config.js
+++ gatsby-theme-patternfly-org/release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "plugins": [
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     ["@semantic-release/git", {
-      "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
+      "assets": ["*","**/*"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
     "@semantic-release/npm"
