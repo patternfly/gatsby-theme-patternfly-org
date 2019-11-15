963014079774f9683d37436ecada1391b0951f15
redallen <zallen@redhat.com>
2019-11-15 15:29:35 -0500

    release correctly

--- gatsby-theme-patternfly-org/release.config.js
+++ gatsby-theme-patternfly-org/release.config.js
@@ -2,10 +2,10 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
     ["@semantic-release/git", {
-      "assets": ["*","**/*"],
+      "assets": ["package.json"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    "@semantic-release/npm"
   ]
 }
