21a77021452d237334c7421c5f51e344a923bd29
redallen <zallen@redhat.com>
2019-11-15 16:03:48 -0500

    fix copy script

--- patternfly-org-demo/copyAssets.js
+++ patternfly-org-demo/copyAssets.js
@@ -1,14 +1,15 @@
 /* eslint-disable no-case-declarations */
-const { copyFileSync } = require('fs');
+const { copySync } = require('fs-extra');
 const { resolve, dirname, join } = require('path');
 const baseCSSFilename = 'patternfly-base.css';
 const stylesDir = resolve(__dirname, './static');
 const pfDir = dirname(require.resolve(`@patternfly/patternfly/${baseCSSFilename}`));
 
+
 const ununsedFontFilesRegExt = /(fa-|\.html$|\.css$)/;
 
-copyFileSync(join(pfDir, 'assets/images'), join(stylesDir, 'assets/images'));
-copyFileSync(join(pfDir, 'assets/fonts'), join(stylesDir, 'assets/fonts'), {
+copySync(join(pfDir, 'assets/images'), join(stylesDir, 'assets/images'));
+copySync(join(pfDir, 'assets/fonts'), join(stylesDir, 'assets/fonts'), {
   filter(src) {
     return !ununsedFontFilesRegExt.test(src);
   }
