0896dc7961169230104d3cc53d5aad1ec8faec2f
evwilkin <evan.wilkinson@gmail.com>
2019-11-15 14:53:30 -0500

    fix(docs): add favicon

--- patternfly-org-demo/gatsby-config.js
+++ patternfly-org-demo/gatsby-config.js
@@ -235,27 +235,7 @@ module.exports = {
         hostname: 'www.patternfly.org'
       },
     },
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        // name is used in the app install prompt
-        name: 'PatternFly 4',
-        // short_name is used on the user's home screen, launcher, or other places where space may be limited
-        short_name: 'PatternFly 4',
-        // The start_url tells the browser where your application should start when it is launched
-        start_url: '/v4',
-        // The background_color property is used on the splash screen when the application is first launched
-        background_color: '#151515',
-        // The theme_color sets the color of the tool bar, and may be reflected in the app's preview in task switchers.
-        theme_color: '#151515',
-        // Opens the web app to look and feel like a standalone native app
-        display: 'standalone',
-        // icon used for splash screen, on home screen
-        icon: 'src/images/patternfly-orb.svg',
-        // do not build favicon from icon image
-        include_favicon: false
-      },
-    },
+    
     // We used to use service workers pre-refactor in gatsby-plugin-offline.
     // We don't need service workers except for a high Lighthouse score. Kill them.
     'gatsby-plugin-remove-serviceworker',
