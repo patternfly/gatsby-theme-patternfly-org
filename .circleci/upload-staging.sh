8ad0ab5362df6195b3c8eeadcf28861e89de2363
redallen <zallen@redhat.com>
2019-11-15 11:20:57 -0500

    add AWS deploy

---
+++ .circleci/upload-staging.sh
@@ -0,0 +1,5 @@
+cd patternfly-org-demo
+yarn run gatsby-plugin-s3 deploy --yes
+aws s3 rm --recursive s3://patternfly-org-staging/v4
+aws s3 sync s3://patternfly-org-preview s3://patternfly-org-staging/v4
+aws cloudfront create-invalidation --distribution-id EQE4NPRH0CQXJ --paths "/v4/*"
