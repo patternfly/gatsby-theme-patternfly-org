eaab06912e1129016eac11c18cbd741a180f3cf0
redallen <zallen@redhat.com>
2019-11-15 09:20:47 -0500

    fix upload script

--- .circleci/upload-docs.sh
+++ .circleci/upload-docs.sh
@@ -22,16 +22,16 @@ DEPLOY_DOMAIN_NEXT="https://${DEPLOY_SUBDOMAIN}-next.surge.sh"
 DEPLOY_DOMAIN_REACT="https://${DEPLOY_SUBDOMAIN}-react.surge.sh"
 DEPLOY_DOMAIN_ORG="https://${DEPLOY_SUBDOMAIN}-org.surge.sh"
 npx surge --project patternfly-org-demo/patternfly-next/public/ --domain ${DEPLOY_DOMAIN_NEXT}
-npx surge --project patternfly-org-demo/patternfly-react/packages/react-docs/public/ --domain ${DEPLOY_DOMAIN_REACT}
+npx surge --project patternfly-org-demo/patternfly-react/packages/patternfly-4/react-docs/public/ --domain ${DEPLOY_DOMAIN_REACT}
 npx surge --project patternfly-org-demo//public/ --domain ${DEPLOY_DOMAIN_ORG}
 
-if [ -n "${PR_NUM}" ] && [ -z "${ALREADY_DEPLOYED}" ] # Leave a Github comment
+if [ -n "${PR_NUM}" ] # && [ -z "${ALREADY_DEPLOYED}" ] # Leave a Github comment
 then
   # Use Issues api instead of PR api because
   # PR api requires comments be made on specific files of specific commits
   GITHUB_PR_COMMENTS="https://api.github.com/repos/${USERNAME}/${REPONAME}/issues/${PR_NUM}/comments"
   echo "Adding github PR comment ${GITHUB_PR_COMMENTS}"
-  curl -H "Authorization: token ${GH_PR_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Core preview: '${DEPLOY_DOMAIN_NEXT}'React preview: '${DEPLOY_DOMAIN_REACT}'Org preview: '${DEPLOY_DOMAIN_ORG}'"}'
+  curl -H "Authorization: token ${GH_PR_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Core preview: '${DEPLOY_DOMAIN_NEXT}'\nReact preview: '${DEPLOY_DOMAIN_REACT}'\nOrg preview: '${DEPLOY_DOMAIN_ORG}'"}'
 else
   echo "Already deployed ${DEPLOY_DOMAIN_NEXT} and ${DEPLOY_DOMAIN_REACT}"
 fi
