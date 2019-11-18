1f68bb29a3ed2941dd36992d6a67e16bf51c7c8c
redallen <zallen@redhat.com>
2019-11-18 11:21:58 -0500

    fix previews

--- .circleci/upload-preview.sh
+++ .circleci/upload-preview.sh
@@ -4,6 +4,8 @@ REPONAME=${CIRCLE_PROJECT_REPONAME}
 PR_NUM=${CIRCLE_PR_NUMBER}
 BRANCH=${CIRCLE_BRANCH}
 
+ORG_TMP_DIR=/tmp/patternfly-org-preview
+
 if [ -n "${PR_NUM}" ] # If build is a PR
 then
   # Domain names follow the RFC1123 spec [a-Z] [0-9] [-] limited to 253 characters
@@ -23,7 +25,10 @@ DEPLOY_DOMAIN_REACT="https://${DEPLOY_SUBDOMAIN}-react.surge.sh"
 DEPLOY_DOMAIN_ORG="https://${DEPLOY_SUBDOMAIN}-org.surge.sh"
 npx surge --project patternfly-org-demo/patternfly-next/public/ --domain ${DEPLOY_DOMAIN_NEXT}
 npx surge --project patternfly-org-demo/patternfly-react/packages/patternfly-4/react-docs/public/ --domain ${DEPLOY_DOMAIN_REACT}
-npx surge --project patternfly-org-demo//public/ --domain ${DEPLOY_DOMAIN_ORG}
+
+mkdir -p ${ORG_TMP_DIR}/v4
+cp -r patternfly-org-demo/public/* ${ORG_TMP_DIR}/v4
+npx surge --project ${ORG_TMP_DIR} --domain ${DEPLOY_DOMAIN_ORG}
 
 if [ -n "${PR_NUM}" ] && [ -z "${ALREADY_DEPLOYED}" ] # Leave a Github comment
 then
@@ -31,7 +36,7 @@ then
   # PR api requires comments be made on specific files of specific commits
   GITHUB_PR_COMMENTS="https://api.github.com/repos/${USERNAME}/${REPONAME}/issues/${PR_NUM}/comments"
   echo "Adding github PR comment ${GITHUB_PR_COMMENTS}"
-  curl -H "Authorization: token ${GH_PR_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Core preview: '${DEPLOY_DOMAIN_NEXT}'\nReact preview: '${DEPLOY_DOMAIN_REACT}'\nOrg preview: '${DEPLOY_DOMAIN_ORG}'"}'
+  curl -H "Authorization: token ${GH_PR_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Core preview: '${DEPLOY_DOMAIN_NEXT}'\nReact preview: '${DEPLOY_DOMAIN_REACT}'\nOrg preview: '${DEPLOY_DOMAIN_ORG}'/v4"}'
 else
-  echo "Already deployed ${DEPLOY_DOMAIN_NEXT} and ${DEPLOY_DOMAIN_REACT}"
+  echo "Already deployed ${DEPLOY_DOMAIN_NEXT}, ${DEPLOY_DOMAIN_REACT}, and ${DEPLOY_DOMAIN_ORG}"
 fi
