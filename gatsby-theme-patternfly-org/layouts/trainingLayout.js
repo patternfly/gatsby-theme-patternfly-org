d7498c1df4a51037df434a4b3e5b495c74f0510f
redallen <zallen@redhat.com>
2019-11-19 12:00:36 -0500

    add katacoda pages

---
+++ gatsby-theme-patternfly-org/layouts/trainingLayout.js
@@ -0,0 +1,87 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { Helmet } from 'react-helmet';
+
+import { Page, PageHeader, Brand, Breadcrumb, BreadcrumbItem } from '@patternfly/react-core';
+import logo from '../images/logo.svg';
+
+const TrainingLayout = ({ katacodaId, location }) => {
+  const data = useStaticQuery(graphql`
+  {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    prInfo: envVars(name: { eq: "PR_INFO" }) {
+      num
+      url
+    }
+    sitePlugin(name: { eq: "gatsby-theme-patternfly-org" }) {
+      pluginOptions {
+        context
+      }
+    }
+  }
+  `);
+  const { title } = data.site.siteMetadata;
+  const { num, url } = data.prInfo;
+  const { context: pageSource } = data.sitePlugin.pluginOptions;
+  
+
+  let headerTitle = title;
+  if (pageSource === 'org') {
+    headerTitle = <Brand src={logo} alt="Patternfly Logo" />;
+  } else if (num) {
+    headerTitle = `PR #${num}`;
+  }
+
+  // TODO: use location
+  const Breadcrumbs = (
+    <Breadcrumb>
+      <BreadcrumbItem to="#">Section Home</BreadcrumbItem>
+      <BreadcrumbItem to="#">Section Title</BreadcrumbItem>
+      <BreadcrumbItem to="#">Section Title</BreadcrumbItem>
+      <BreadcrumbItem to="#" isActive>
+        Section Landing
+      </BreadcrumbItem>
+    </Breadcrumb>
+  );
+  
+  const Header = (
+    <PageHeader
+      className="ws-page-header"
+      logo={headerTitle}
+      logoProps={{
+        href: url || '/'
+      }}
+      showNavToggle
+      toolbar={Breadcrumbs}
+    />
+  );
+
+  // Wrap in a div to force scrolling the same content
+  // TODO: SEO
+  return (
+    <div>
+      <Helmet>
+        <title>{title}</title>
+        <script src="//katacoda.com/embed.js"></script>
+      </Helmet>
+      <Page header={Header}>
+        <div id="katacoda-scenario-1"
+          data-katacoda-id={katacodaId}
+          data-katacoda-color="004d7f"
+          style={{ height: '600px', paddingTop: '20px'}} />
+      </Page>
+    </div>
+  );
+}
+
+export default TrainingLayout;
--- gatsby-theme-patternfly-org/templates/mdx.js
+++ gatsby-theme-patternfly-org/templates/mdx.js
@@ -8,6 +8,7 @@ import {
   Title
 } from '@patternfly/react-core';
 import SideNavLayout from '../layouts/sideNavLayout';
+import TrainingLayout from '../layouts/trainingLayout';
 import AutoLinkHeader from '../components/autoLinkHeader';
 import Example from '../components/example';
 import CSSVariables from '../components/cssVariables';
@@ -47,6 +48,7 @@ const getSourceTitle = source => {
 }
 
 export default ({ data, location, pageContext }) => {
+  console.log('location', location);
   const { cssPrefix, hideTOC, experimentalStage, optIn, hideDarkMode, showTitle } = data.doc.frontmatter;
   const { componentName, navSection } = data.doc.fields;
   const { title, source, tableOfContents, htmlExamples, propComponents = [''], showBanner } = pageContext;
@@ -196,26 +198,30 @@ export default ({ data, location, pageContext }) => {
   );
 
   return (
-    <SideNavLayout
-      location={location}
-      context={source}
-      parityComponent={parityComponent}
-      showBanner={showBanner}
-    >
-      <PageSection className="ws-section">
+    location.state.katacodaId
+      ? <TrainingLayout location={location} katacodaId={location.state.katacodaId} />
+      : (
+      <SideNavLayout
+        location={location}
+        context={source}
+        parityComponent={parityComponent}
+        showBanner={showBanner}
+      >
+        <PageSection className="ws-section">
 
-        <TableOfContents />
+          <TableOfContents />
 
-        {/* Wrap in div for :last-child CSS selectors */}
-        <div>
-          <MDXContent />
-        </div>
+          {/* Wrap in div for :last-child CSS selectors */}
+          <div>
+            <MDXContent />
+          </div>
 
-        {props.length > 0 && <PropsSection />}
+          {props.length > 0 && <PropsSection />}
 
-        {cssPrefix && <CSSVariablesSection />}
-      </PageSection>
-    </SideNavLayout>
+          {cssPrefix && <CSSVariablesSection />}
+        </PageSection>
+      </SideNavLayout>
+        )
   );
 }
 
