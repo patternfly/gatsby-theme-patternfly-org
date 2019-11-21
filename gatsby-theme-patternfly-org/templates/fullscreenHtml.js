7b05a0cf9f7da800be74c643cfce6a5a23482693
zallen <47335686+redallen@users.noreply.github.com>
2019-11-21 13:32:55 -0500

    Fix/a11y (#121)
    * going
    
    * add title
    
    * add title
    
    * use new patternfly-a11y
    
    * work thing
    
    * test in ci
    
    * add back chromedriver
    
    * remove chromedriver
    
    * sort fullscreen pages
    
    * force new install
    
    * restore JS cache
    
    * restore cache in right place
    
    * persist json file

--- gatsby-theme-patternfly-org/templates/fullscreenHtml.js
+++ gatsby-theme-patternfly-org/templates/fullscreenHtml.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import './fullscreen.css';
 
 const FullscreenHTMLTemplate = ({ pageContext }) => (
-  <div
-    class="ws-fullscreen-example"
-    dangerouslySetInnerHTML={{__html: pageContext.code || 'No fullscreen example.'}} />
+  <main>
+    <Helmet>
+      <title>{pageContext.title}</title>
+    </Helmet>
+    <div
+      className="ws-fullscreen-example"
+      dangerouslySetInnerHTML={{__html: pageContext.code || 'No fullscreen example.'}} />
+  </main>
 );
 
 export default FullscreenHTMLTemplate;
